f83e5f27acf0cdeb22b82ac4378ca1fccb3220f1
Jiri Slaby <jslaby@suse.cz>
2012-06-09 22:46:05 +0200

    kmodsrc: fix stop_machine expectation
    stop_machine is not performed in a kstop thread since 2.6.35
    (46cb327161c7958), but in migration. So fix this in is_stop_machine().
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- kmodsrc/ksplice.c
+++ kmodsrc/ksplice.c
@@ -3307,7 +3307,9 @@ static abort_t check_record(struct conflict_addr *ca,
 static bool is_stop_machine(const struct task_struct *t)
 {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,27)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,28)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,35)
+	const char *kstop_prefix = "migration/";
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,28)
 	const char *kstop_prefix = "kstop/";
 #else /* LINUX_VERSION_CODE < */
 /* c9583e55fa2b08a230c549bd1e3c0bde6c50d9cc was after 2.6.27 */
