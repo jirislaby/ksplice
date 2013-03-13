jirislaby/ksplice

commit 59647168dd7cab3e199b9c11f2bf2f2d89f46900

kmodsrc: support RONX .text
When CONFIG_DEBUG_SET_MODULE_RONX is set, the text of modules is not
writable and patching such memory causes an oops. Fix that by marking
.text RW temporarily during patching.

Note that we cannot call the function that marks .text RW before we
bootstrap as it is not exported and EXTRACT_SYMBOL does not work in
bootstrap phase yet. So we instead find that one specific function and
use that.

Signed-off-by: Jiri Slaby <jslaby@suse.cz>

kmodsrc/ksplice.c
@@ -4277,9 +4277,13 @@ static int maybe_cleanup_ksplice_update_wrapper(void *updateptr)
 	return 0;
 }
 
+EXTRACT_SYMBOL(set_all_modules_text_ro);
+EXTRACT_SYMBOL(set_all_modules_text_rw);
+
 static ssize_t stage_store(struct update *update, const char *buf, size_t len)
 {
 	enum stage old_stage;
+	set_all_modules_text_rw();
 	mutex_lock(&module_mutex);
 	old_stage = update->stage;
 	if ((strncmp(buf, "applied", len) == 0 ||
@@ -4297,6 +4301,7 @@ static ssize_t stage_store(struct update *update, const char *buf, size_t len)
 			    "ksplice_cleanup_%s", update->kid);
 
 	mutex_unlock(&module_mutex);
+	set_all_modules_text_ro();
 	return len;
 }
 
@@ -4378,7 +4383,11 @@ static struct ksplice_mod_change bootstrap_mod_change = {
 static int init_ksplice(void)
 {
 #ifdef KSPLICE_STANDALONE
+	typeof(&set_all_modules_text_rw) __my_set_all_modules_text_rw;
 	struct ksplice_mod_change *change = &bootstrap_mod_change;
+	LIST_HEAD(vals);
+	abort_t ret;
+
 	change->update = init_ksplice_update(change->kid);
 	sort(change->new_code.system_map,
 	     change->new_code.system_map_end - change->new_code.system_map,
@@ -4386,9 +4395,25 @@ static int init_ksplice(void)
 	if (change->update == NULL)
 		return -ENOMEM;
 	add_to_update(change, change->update);
+
+	/* we can call it directly even after apply_relocs below */
+	ret = add_system_map_candidates(change, change->new_code.system_map,
+			change->new_code.system_map_end,
+			"set_all_modules_text_rw", &vals);
+	if (ret != OK) {
+		/* FIXME leak */
+		return -EIO;
+	}
+	__my_set_all_modules_text_rw =
+		list_entry(vals.next, struct candidate_val, list)->val;
+	release_vals(&vals);
+
+	__my_set_all_modules_text_rw();
 	change->update->debug = debug;
 	change->update->abort_cause =
 	    apply_relocs(change, ksplice_init_relocs, ksplice_init_relocs_end);
+	set_all_modules_text_ro();
+
 	if (change->update->abort_cause == OK)
 		bootstrapped = true;
 	cleanup_ksplice_update(bootstrap_mod_change.update);

commit 0ac94c36424ce80216b2b9e76bf711a3aac49095

fix set_all_modules_text_rw fallout
When CONFIG_DEBUG_SET_MODULE_RONX is not set, set_all_modules_text_rw
is an inline function. So we are trying to find address of a ghost. Do
that only if DEBUG_SET_MODULE_RONX is set.

This is a regression reported by Richard and which I introduced in
59647168dd7cab3e199b9c11f2bf2f2d89f46900 (kmodsrc: support RONX
.text).

Signed-off-by: Jiri Slaby <jslaby@suse.cz>
Reported-and-Tested-by: Richard Weinberger <richard@nod.at>

kmodsrc/ksplice.c
@@ -3480,15 +3480,39 @@ static struct ksplice_mod_change bootstrap_mod_change = {
 	.new_code.system_map = ksplice_system_map,
 	.new_code.system_map_end = ksplice_system_map_end,
 };
+
+static int __my_set_all_modules_text_rw(struct ksplice_mod_change *change)
+{
+#ifdef CONFIG_DEBUG_SET_MODULE_RONX
+	typeof(&set_all_modules_text_rw) fun;
+	LIST_HEAD(vals);
+	abort_t ret;
+
+	ret = add_system_map_candidates(change, change->new_code.system_map,
+			change->new_code.system_map_end,
+			"set_all_modules_text_rw", &vals);
+	if (ret != OK)
+		return -EIO;
+	fun = list_entry(vals.next, struct candidate_val, list)->val;
+	release_vals(&vals);
+
+	fun();
+#else
+	/*
+	 * It's an inline, no problem to call it directly.
+	 * It should be a nop though.
+	 */
+	set_all_modules_text_rw();
+#endif
+	return 0;
+}
 #endif /* KSPLICE_STANDALONE */
 
 static int init_ksplice(void)
 {
 #ifdef KSPLICE_STANDALONE
-	typeof(&set_all_modules_text_rw) __my_set_all_modules_text_rw;
 	struct ksplice_mod_change *change = &bootstrap_mod_change;
-	LIST_HEAD(vals);
-	abort_t ret;
+	int ret;
 
 	change->update = init_ksplice_update(change->kid);
 	sort(change->new_code.system_map,
@@ -3498,19 +3522,12 @@ static int init_ksplice(void)
 		return -ENOMEM;
 	add_to_update(change, change->update);
 
-	/* we can call it directly even after apply_relocs below */
-	ret = add_system_map_candidates(change, change->new_code.system_map,
-			change->new_code.system_map_end,
-			"set_all_modules_text_rw", &vals);
-	if (ret != OK) {
+	/* we can call it directly even after apply_relocs */
+	ret = __my_set_all_modules_text_rw(change);
+	if (ret) {
 		/* FIXME leak */
-		return -EIO;
+		return ret;
 	}
-	__my_set_all_modules_text_rw =
-		list_entry(vals.next, struct candidate_val, list)->val;
-	release_vals(&vals);
-
-	__my_set_all_modules_text_rw();
 	change->update->debug = debug;
 	change->update->abort_cause =
 	    apply_relocs(change, ksplice_init_relocs, ksplice_init_relocs_end);